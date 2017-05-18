aac0ec32ba531365ff77b5387649b455710f8bb6
Danil Akhmetov <dakhmetov@mirantis.com>
2017-05-18 19:21:55 +0300

    refacotr

--- deadpbrewery.go
+++ deadpbrewery.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"fmt"
-        "os"
+	"encoding/json"
+	"os"
 	"io/ioutil"
+	"fmt"
 	"github.com/boltdb/bolt"
+	"time"
+	"strconv"
 )
 
 func main() {
-        timestamp := os.Args[1]
-        dump_file := os.Args[2]
-        db_name := os.Args[3]
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage:\n  %s dump_file db_file\n", os.Args[0])
+		os.Exit(0)
+	}
 
-	dump_data, err := ioutil.ReadFile(dump_file)
+	fname := os.Args[1]
+	db_name := os.Args[2]
+
+	reader, err := os.Open(fname)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Filed to open dump file: %s\n", string(fname))
 		return
 	}
 
@@ -22,14 +29,36 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	fmt.Printf("timestamp: %s json_snapshot_file: %s\n", string(timestamp), string(dump_file))
-
-	db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucketIfNotExists([]byte("Snapshots"))
-		b := tx.Bucket([]byte("Snapshots"))
-                err := b.Put([]byte(timestamp), dump_data)
-		return err
-	})
 	defer db.Close()
+
+	decoder := json.NewDecoder(reader)
+	target := make([]interface{}, 0)
+	err = decoder.Decode(&target)
+	if err != nil {
+		fmt.Printf("Appending raw data\n")
+		stamp := strconv.FormatInt(time.Now().Unix(), 10)
+		fmt.Printf("%s\n", stamp)
+		dump_data, _ := ioutil.ReadFile(fname)
+                db.Update(func(tx *bolt.Tx) error {
+                        tx.CreateBucketIfNotExists([]byte("Snapshots"))
+                        b := tx.Bucket([]byte("Snapshots"))
+                        err := b.Put([]byte(stamp), dump_data)
+                        return err
+                })
+	os.Exit(0)
+	}
+
+	fmt.Printf("Building from scratch\n")
+	for _, item := range target {
+		top := item.(map[string]interface{})
+		stamp := top["timestamp"].(string)
+		fmt.Printf("%s\n", stamp)
+		data, _ := json.Marshal(top["snapshot"])
+		db.Update(func(tx *bolt.Tx) error {
+			tx.CreateBucketIfNotExists([]byte("Snapshots"))
+			b := tx.Bucket([]byte("Snapshots"))
+			err := b.Put([]byte(stamp), data)
+			return err
+		})
+	}
 }
